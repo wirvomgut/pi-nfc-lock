d84b31400192f2037de5f3e3cb71ecf36f0dfa27
Julian Liebl <jujulian1987@gmail.com>
2016-11-03 23:28:56 +0100

    fixed false line endings

--- pi-nfc-lock.py
+++ pi-nfc-lock.py
@@ -7,14 +7,14 @@ import signal
 import hashlib
 import subprocess
 import RPi.GPIO as GPIO
-            
+
 log = logging.getLogger("wvg_key")
 known_uid_to_name_dict = {}
-                                    
+
 #################
 # CLASSES       #
 #################
-class Timeout(Exception):                                                                         
+class Timeout(Exception):
     pass
 
 #################
@@ -23,7 +23,7 @@ class Timeout(Exception):
 
 def init_logging():
     # define logging information and timestamp format
-    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s", "%Y-%m-%d %H:%M:%S$
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s", "%Y-%m-%d %H:%M:%S")
 
     # create logger
     log.setLevel(logging.DEBUG)
@@ -31,7 +31,7 @@ def init_logging():
     # create console handler
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    
+
     # create file handler
     fh = logging.FileHandler('./wvg-lock.log')
     fh.setLevel(logging.DEBUG)
@@ -41,19 +41,21 @@ def init_logging():
     fh.setFormatter(formatter)
     log.addHandler(ch)
     log.addHandler(fh)
+
                                   
 def parse_allowed_uids():
     with open('accessAllowed.csv', 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in reader:
             known_uid_to_name_dict.update({row[0]: row[1]})
-
+                
 def signal_timeout(signal, frame):
     raise Timeout
 
 def signal_handler(signal, frame):
     sys.exit(0)
-                                                                                              
+
+
 def on_known():
     GPIO.output(38,1)
     time.sleep(5)
@@ -64,42 +66,42 @@ def on_unknown():
 
 def on_shutdown():
     log.info("System shutdown")
-
+    
 
 #################
 # INIT          #
 #################
 
 init_logging()
-
+    
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(38, GPIO.OUT)
-
+    
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGALRM, signal_timeout)
 atexit.register(on_shutdown)
-
-parse_allowed_uids()
     
+parse_allowed_uids()
+
 log.info("System startup: " + `len(known_uid_to_name_dict)` + " known Uid(s)")
 
 #################
-# LOGIC         #                                                                             
+# LOGIC         #
 #################
     
 while True:
     try:
         signal.alarm(3)
-        uid = str(subprocess.Popen(["python", "./get_uid.py"], stdout=subprocess.PIPE).commun$
+        uid = str(subprocess.Popen(["python", "./get_uid.py"], stdout=subprocess.PIPE).communicate()[0]).rstrip()
         signal.alarm(0)
         if uid:
             if uid in known_uid_to_name_dict.keys():
-                log.info("Access granted for " + known_uid_to_name_dict[uid] + " [" + uid[0:1$
+                log.info("Access granted for " + known_uid_to_name_dict[uid] + " [" + uid[0:15] + "]")
                 on_known()
             else:
                 log.warn("No Access " + uid)
                 on_unknown()
     except:
         # We do not want an unknown exception to kill our python script.
-        log.error("Unexpected error: " + str(sys.exc_info()[0]))                                      
+        log.error("Unexpected error: " + str(sys.exc_info()[0]))
         time.sleep(60)
