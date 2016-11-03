5515081d67c622f4b0f4291093439d0391ff164c
Julian Liebl <jujulian1987@gmail.com>
2016-11-03 23:08:33 +0100

    using external script to read nfc tag instead
    - much faster
    - more stable

--- pi-nfc-lock.py
+++ pi-nfc-lock.py
@@ -1,15 +1,21 @@
 import csv
 import logging
-import nxppy
 import time
 import atexit
 import sys
 import signal
 import hashlib
-
+import subprocess
+import RPi.GPIO as GPIO
+            
 log = logging.getLogger("wvg_key")
 known_uid_to_name_dict = {}
-
+                                    
+#################
+# CLASSES       #
+#################
+class Timeout(Exception):                                                                         
+    pass
 
 #################
 # METHODS       #
@@ -17,7 +23,7 @@ known_uid_to_name_dict = {}
 
 def init_logging():
     # define logging information and timestamp format
-    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s", "%Y-%m-%d %H:%M:%S")
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s", "%Y-%m-%d %H:%M:%S$
 
     # create logger
     log.setLevel(logging.DEBUG)
@@ -25,30 +31,36 @@ def init_logging():
     # create console handler
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
+    
+    # create file handler
+    fh = logging.FileHandler('./wvg-lock.log')
+    fh.setLevel(logging.DEBUG)
 
     # link logger formatter and handler
     ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
     log.addHandler(ch)
-
-
+    log.addHandler(fh)
+                                  
 def parse_allowed_uids():
     with open('accessAllowed.csv', 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in reader:
             known_uid_to_name_dict.update({row[0]: row[1]})
 
+def signal_timeout(signal, frame):
+    raise Timeout
 
 def signal_handler(signal, frame):
     sys.exit(0)
-
-
+                                                                                              
 def on_known():
-    pass
-
+    GPIO.output(38,1)
+    time.sleep(5)
+    GPIO.output(38,0)
 
 def on_unknown():
-    pass
-
+    time.sleep(2)
 
 def on_shutdown():
     log.info("System shutdown")
@@ -60,34 +72,34 @@ def on_shutdown():
 
 init_logging()
 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(38, GPIO.OUT)
+
 signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGALRM, signal_timeout)
 atexit.register(on_shutdown)
 
 parse_allowed_uids()
-
-mifare = nxppy.Mifare()
-
+    
 log.info("System startup: " + `len(known_uid_to_name_dict)` + " known Uid(s)")
 
 #################
-# LOGIC         #
+# LOGIC         #                                                                             
 #################
-
+    
 while True:
     try:
-        uid = hashlib.sha224(mifare.select()).hexdigest()
-        if uid in known_uid_to_name_dict.keys():
-            log.info("Access granted for " + known_uid_to_name_dict[uid] + " [" + uid[0:15] + "]")
-            on_known()
-        else:
-            log.warn("No Access " + uid)
-            on_unknown()
-    except nxppy.SelectError:
-        # SelectError is raised if no card is in the field.
-        pass
+        signal.alarm(3)
+        uid = str(subprocess.Popen(["python", "./get_uid.py"], stdout=subprocess.PIPE).commun$
+        signal.alarm(0)
+        if uid:
+            if uid in known_uid_to_name_dict.keys():
+                log.info("Access granted for " + known_uid_to_name_dict[uid] + " [" + uid[0:1$
+                on_known()
+            else:
+                log.warn("No Access " + uid)
+                on_unknown()
     except:
         # We do not want an unknown exception to kill our python script.
-        log.error("Unexpected error: " + sys.exc_info()[0])
-        pass
-
-    time.sleep(1)
+        log.error("Unexpected error: " + str(sys.exc_info()[0]))                                      
+        time.sleep(60)
